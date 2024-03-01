7fca8c3e525bc8195cd2ac21ab48ab1ac06b2881
yuzuuu <takahiroi.311@gmail.com>
2024-03-01 12:40:39 +0900

    postgresqlにデータを保存するように修正

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -1,34 +1,33 @@
 # frozen_string_literal: true
 
+require 'pg'
+
 module MemoHelper
-  def read_memos_json
-    File.open('memos.json', 'r') do |file|
-      JSON.parse(file.read)
-    end
+  def all_memos
+    connection = PG.connect(dbname: 'my-app')
+    connection.exec('SELECT * FROM my_memos')
   end
 
-  def init_memos_json
-    File.open('memos.json', 'w') do |file|
-      JSON.dump([], file)
-    end
+  def find_memo(id)
+    connection = PG.connect(dbname: 'my-app')
+    memos = connection.exec('SELECT * FROM my_memos where id = $1 limit 1', [id])
+    memos.first
   end
 
-  def write_memos_json(memos)
-    File.open('memos.json', 'w') do |file|
-      JSON.dump(memos, file)
-    end
+  def create_memo(params)
+    title = params[:title] || 'サンプルタイトル'
+    body =  params[:body] || 'サンプルの内容'
+    connection = PG.connect(dbname: 'my-app')
+    connection.exec_params('INSERT INTO my_memos (title, body) VALUES ($1, $2)', [title, body])
   end
 
-  def memo(id)
-    memos = []
-    File.open('memos.json', 'r') do |file|
-      memos = JSON.parse(file.read)
-    end
-    memos[id.to_i]
+  def update_memo(params, id)
+    connection = PG.connect(dbname: 'my-app')
+    connection.exec_params('UPDATE my_memos SET title = ($1), body = ($2) WHERE id=($3)', [params[:title], params[:body], id])
   end
 
-  def memos
-    init_memos_json unless File.exist?('memos.json')
-    read_memos_json
+  def delete_memo(id)
+    connection = PG.connect(dbname: 'my-app')
+    connection.exec_params('DELETE FROM my_memos WHERE id = $1', [id])
   end
 end
