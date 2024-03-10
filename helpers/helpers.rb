19c609b39140494ee5438182b0f79b0128dbc442
yuzuuu <takahiroi.311@gmail.com>
2024-03-10 18:01:44 +0900

    Revert "connectionを別メソッドに切り出し、メモ化する"
    This reverts commit bdb1a25e11ca974bee1de9234bc0a570efba3971.

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -4,10 +4,12 @@ require 'pg'
 
 module MemoHelper
   def all_memos
+    connection = PG.connect(dbname: 'my-app')
     connection.exec('SELECT * FROM my_memos')
   end
 
   def find_memo(id)
+    connection = PG.connect(dbname: 'my-app')
     memos = connection.exec('SELECT * FROM my_memos where id = $1 limit 1', [id])
     memos.first
   end
@@ -15,20 +17,17 @@ module MemoHelper
   def create_memo(params)
     title = params[:title] || 'サンプルタイトル'
     body =  params[:body] || 'サンプルの内容'
+    connection = PG.connect(dbname: 'my-app')
     connection.exec_params('INSERT INTO my_memos (title, body) VALUES ($1, $2)', [title, body])
   end
 
   def update_memo(params, id)
+    connection = PG.connect(dbname: 'my-app')
     connection.exec_params('UPDATE my_memos SET title = ($1), body = ($2) WHERE id=($3)', [params[:title], params[:body], id])
   end
 
   def delete_memo(id)
+    connection = PG.connect(dbname: 'my-app')
     connection.exec_params('DELETE FROM my_memos WHERE id = $1', [id])
-  end
-
-  private
-
-  def connection
-    @connection ||= PG.connect(dbname: 'mydb')
   end
 end
